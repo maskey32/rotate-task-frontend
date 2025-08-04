6130a0bacaab3657894cea73d1ac6ec08178405a
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2025-08-04 17:17:26 +0100

    chore(DashboardPage): change component name from  Dashboard to DashboardPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { dashboardStyles } from "../styles/dashboard";
 import { LeftPane, RightPane } from "../components/Dashboard/main";
 
-export default function Dashboard() {
+export default function DashboardPage() {
   return (
     <main className={dashboardStyles.container}>
       <div className={dashboardStyles.content}>
